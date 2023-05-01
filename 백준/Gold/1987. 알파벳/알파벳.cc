49e490f75960b28db758e3fe91ec4c1a26bf6d1d
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-05-01 12:46:25 +0900

    [Gold IV] Title: 알파벳, Time: 392 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -2,11 +2,6 @@
 #include <stack>
 using namespace std;
 
-struct alpha {
-	string s;
-	int r, c;
-};
-
 // 상하좌우 탐색을 위함
 int dr[4] = { -1, 0, 1, 0 };
 int dc[4] = { 0, 1, 0, -1 };
@@ -15,18 +10,22 @@ char arr[20][20];
 int visit[26];
 int n, m, re;
 
+// DFS
 void DFS(int r, int c, int cnt) {
-
 	visit[arr[r][c]] = 1;
 	re = max(re, cnt);
 
+	// 상하좌우 탐색
 	for (int i = 0; i < 4; i++) {
 		int rr = r + dr[i];
 		int cc = c + dc[i];
 
+		// 범위를 넘어서면 continue
 		if (rr < 0 || cc < 0 || rr >= n || cc >= m) continue;
+		// 지나온 경로 중 해당 알파벳을 밟은 적이 있다면 continue
 		if (visit[arr[rr][cc]]) continue;
 
+		// 재귀
 		DFS(rr, cc, cnt + 1);
 		visit[arr[rr][cc]] = 0;
 	}
@@ -39,11 +38,12 @@ int main() {
 
 	cin >> n >> m;
 
-	for (int i = 0; i < n; i++)  
+	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++) {
 			cin >> arr[i][j];
 			arr[i][j] -= 'A';
 		}
+
 	DFS(0, 0, 1);
 
 	cout << re;
