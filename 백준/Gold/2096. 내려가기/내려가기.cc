07e41f6503b5db547532d14804a36c1313498607
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-05-09 01:06:53 +0900

    [Gold V] Title: 내려가기, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int n,ma[3],mi[3],a,b,c,x,y,z;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin>>n;
+	cin>>a>>b>>c;
+	ma[0]=mi[0]=a; ma[1]=mi[1]=b; ma[2]=mi[2]=c;
+	
+	for (int i=1;i<n;i++) {
+		cin>>a>>b>>c;
+		x=ma[0]; y=ma[1], z=ma[2];
+		ma[0]=a+max(x,y);
+		ma[1]=b+max(x,max(y,z));
+		ma[2]=c+max(y,z);
+		
+		x=mi[0]; y=mi[1], z=mi[2];
+		mi[0]=a+min(x,y);
+		mi[1]=b+min(x,min(y,z));
+		mi[2]=c+min(y,z);
+	}
+	
+	cout<<max(ma[0],max(ma[1],ma[2]))<<" "<<min(mi[0],min(mi[1],mi[2]));
+}
