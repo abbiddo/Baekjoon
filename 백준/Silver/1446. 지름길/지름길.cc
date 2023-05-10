85aadcf6bdef3214b396612daddc778b15e9704f
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-05-10 10:58:47 +0900

    [Silver I] Title: 지름길, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/1446. 지름길/지름길.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int arr[12][3];
+int dp[10001];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n, d, c = 0;   cin >> n >> d;
+    for (int i = 0; i < n; i++) cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
+
+    for (int i = 1; i <= d; i++) {
+        for (int j = 0; j < n; j++) {
+            if (arr[j][1] == i) {
+                if (dp[i]) dp[i] = min(dp[i], min(dp[arr[j][0]] + arr[j][2], dp[i - 1] + 1));
+                else dp[i] = min(dp[arr[j][0]] + arr[j][2], dp[i - 1] + 1);
+                c = 1;
+            }
+        }
+        if (!c) dp[i] = dp[i - 1] + 1;
+        c = 0;
+    }
+
+    cout << dp[d];
+}
