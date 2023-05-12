d29d016a9f71b320abf4e999e3c10c833de90484
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-05-13 04:07:45 +0900

    [Silver II] Title: 5차 전직, Time: 196 ms, Memory: 4364 KB -BaekjoonHub

---
+++ 백준/Silver/16112. 5차 전직/5차 전직.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#define ll long long
+using namespace std;
+
+ll arr[300000];
+int main() {
+	int n, m;	cin >> n >> m;
+	ll sum = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		sum += arr[i];
+	}
+	sum *= m;
+	sort(arr, arr + n);
+
+	int index = 0;
+	for (int i = 0; i < m; i++) 
+		sum -= arr[index++] * (m - i);
+
+	cout << sum;
+}
