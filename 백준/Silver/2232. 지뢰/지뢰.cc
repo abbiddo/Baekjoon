b1c992bc35b2a2327704d0ebd69450b8b2f15d33
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-05-13 23:55:08 +0900

    [Silver II] Title: 지뢰, Time: 20 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/2232. 지뢰/지뢰.cc
+++ 백준/Silver/2232. 지뢰/지뢰.cc
@@ -1,19 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int arr[50000];
+int arr[50002];
 
 int main() {
 	int n;	cin >> n;
-	for (int i = 0; i < n; i++) cin >> arr[i];
+	for (int i = 1; i <= n; i++) cin >> arr[i];
 
-	if (n == 1) {
-		cout << 1;
-		return 0;
-	}
-
-	if (arr[0] >= arr[1]) cout << 1 << "\n";
-	for (int i = 1; i < n - 1; i++) 
-		if (arr[i - 1] <= arr[i] && arr[i + 1] <= arr[i]) cout << i + 1 << "\n";
-	if (arr[n - 1] >= arr[n - 2]) cout << n;
+	for (int i = 1; i <= n; i++) 
+		if (arr[i - 1] <= arr[i] && arr[i + 1] <= arr[i]) 
+			cout << i << "\n";
 }
