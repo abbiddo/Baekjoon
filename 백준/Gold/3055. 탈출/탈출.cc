559ce494a96f1b1a0d6eafc0170ceb835c239989
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-05-20 22:20:34 +0900

    [Gold IV] Title: 탈출, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.cc
+++ 백준/Gold/3055. 탈출/탈출.cc
@@ -4,77 +4,69 @@ using namespace std;
 
 int n, m;
 char board[50][50];
-bool pisit[50][50];	// 사람의 방문 체크
-bool fisit[50][50];	// 불의 방문 체크
-// 상하좌우 탐색을 위함
+bool gisit[50][50];	// 고슴도치의 방문 체크
+bool wisit[50][50];	// 물의 방문 체크
+
 int dr[4] = { -1, 0, 1, 0 };
 int dc[4] = { 0, 1, 0, -1 };
 
-queue<pair<int, int>> pq;	// 사람 큐
-queue<pair<int, int>> fq;	// 불 큐
+queue<pair<int, int>> gq;
+queue<pair<int, int>> wq;	
 
 void BFS() {
 	int cnt = 0;
 
-	// 큐가 빌 때까지 반복
-	while (!pq.empty()) {
+	while (!gq.empty()) {
 		cnt++;
 
-		int fize = fq.size();
-		int pize = pq.size();
+		int wize = wq.size();
+		int gize = gq.size();
 
-		for (int i = 0; i < fize; i++) {
-			int fr = fq.front().first;
-			int fc = fq.front().second;
+		for (int i = 0; i < wize; i++) {
+			int wr = wq.front().first;
+			int wc = wq.front().second;
 
-			fq.pop();
+			wq.pop();
 
 			for (int j = 0; j < 4; j++) {
-				int r = fr + dr[j];
-				int c = fc + dc[j];
-
-				// 범위를 벗어나면 continue
+				int r = wr + dr[j];
+				int c = wc + dc[j];
+				
 				if (r < 0 || c < 0 || r >= n || c >= m) continue;
-				// 방문한 적이 있으면 continue
-				if (fisit[r][c]) continue;
-				// 벽이면 continue
-				if (board[r][c] == '#') continue;
+				if (wisit[r][c]) continue;
+				if (board[r][c] == '*') continue;
+				if (board[r][c] == 'D') continue;
 
-				if (board[r][c] == 'o') continue;
-
-				fq.push({ r,c });
-				fisit[r][c] = 1;
-				board[r][c] = '#';
+				wq.push({ r,c });
+				wisit[r][c] = 1;
+				board[r][c] = '*';
 			}
 		}
 
-		for (int i = 0; i < pize; i++) {
-			int pr = pq.front().first;
-			int pc = pq.front().second;
+		for (int i = 0; i < gize; i++) {
+			int gr = gq.front().first;
+			int gc = gq.front().second;
 
-			pq.pop();
+			gq.pop();
 
 			for (int j = 0; j < 4; j++) {
-				int r = pr + dr[j];
-				int c = pc + dc[j];
+				int r = gr + dr[j];
+				int c = gc + dc[j];
 
-				// 범위를 벗어나면 continue
 				if (r < 0 || c < 0 || r >= n || c >= m) continue;
-				// 방문한 적이 있으면 continue
-				if (pisit[r][c]) continue;
-				// 벽이면 continue
-				if (board[r][c] == '#') continue;
+				if (gisit[r][c]) continue;
+				if (board[r][c] == '*') continue;
 
-				// 탈출 가능하면 출력 후 종료
-				if (board[r][c] == 'o') {
+				if (board[r][c] == 'D') {
 					cout << cnt;
 					return;
 				}
-				pq.push({ r,c });
-				pisit[r][c] = 1;
+				gq.push({ r,c });
+				gisit[r][c] = 1;
 			}
 		}
 	}
+
 	cout << "KAKTUS";
 	return;
 }
@@ -90,23 +82,19 @@ int main() {
 		for (int j = 0; j < m; j++) {
 			cin >> board[i][j];
 
-			// 불 입력 시 불 큐에 추가, 벽과 같이 갈 수 없는 공간으로 취급
 			if (board[i][j] == '*') {
-				fq.push({ i, j });
-				fisit[i][j] = 1;
-				board[i][j] = '#';
+				wq.push({ i, j });
+				wisit[i][j] = 1;
 			}
 
-			// 사람 입력 시 사람 큐에 추가
 			else if (board[i][j] == 'S') {
-				pq.push({ i, j });
-				pisit[i][j] = 1;
+				gq.push({ i, j });
+				gisit[i][j] = 1;
 				board[i][j] = '.';
 			}
 
-			else if (board[i][j] == 'X') board[i][j] = '#';
-
-			else if (board[i][j] == 'D') board[i][j] = 'o';
+			// 돌
+			else if (board[i][j] == 'X') board[i][j] = '*';
 		}
 
 	BFS();
