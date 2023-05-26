71c79d3f70f70402635462cab3509b283628975d
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-05-26 09:43:32 +0900

    [Gold IV] Title: 네트워크 연결, Time: 36 ms, Memory: 4460 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int visit[1001];
+vector<pair<int, pair<int, int>>> v;
+
+int getParent(int x) {
+	if (visit[x] == x) return x;
+	return visit[x] = getParent(visit[x]);
+}
+
+void unionParent(int a, int b) {
+	a = getParent(a);
+	b = getParent(b);
+
+	if (a < b) visit[b] = a;
+	else visit[a] = b;
+}
+
+bool findParent(int a, int b) {
+	a = getParent(a);
+	b = getParent(b);
+
+	if (a == b) return 1;
+	return 0;
+}
+
+int main() {
+	// 입출력 속도를 단축시키기 위함
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, m;	cin >> n >> m;
+	int connect = 0;
+
+	for (int i = 1; i <= n; i++) 
+		visit[i] = i;
+
+	for (int i = 0; i < m; i++) {
+		int a, b, c;	cin >> a >> b >> c;
+		v.push_back({ c, {a, b} });
+	}
+
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < m; i++) {
+		if (!findParent(v[i].second.first, v[i].second.second)) {
+			connect += v[i].first;
+			unionParent(v[i].second.first, v[i].second.second);
+		}
+	}
+	cout << connect;
+}
