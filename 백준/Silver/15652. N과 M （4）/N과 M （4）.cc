99ecbbd746f38b71e45941d307402aec2f2d0dc6
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-05-29 05:00:05 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int n,m;
+int arr[9];
+
+void dfs(int num, int cnt){
+	if (cnt==m){
+		for (int i=0;i<m;i++) cout<<arr[i]<<" ";
+		cout<<"\n";
+		return;
+	}
+	for (int i=num;i<=n;i++){
+		arr[cnt]=i;
+		dfs(i,cnt+1);
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin>>n>>m;
+	dfs(1,0);
+}
