d00c11aca78ec83443d3e4d03af10a1a5d247112
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-05-29 15:45:12 +0900

    [Gold V] Title: 보물섬, Time: 128 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int n, m, dist;
+char arr[50][50];
+
+int dr[4] = { -1, 0, 1, 0 };
+int dc[4] = { 0, 1, 0, -1 };
+
+struct treasure {
+	int r, c, cnt;
+};
+
+void BFS(int i, int j) {
+	queue<treasure> q;
+	bool visit[50][50] = { 0 };
+
+	q.push({ i, j, 0 });
+	visit[i][j] = true;
+
+	while (!q.empty()) {
+		int r = q.front().r;
+		int c = q.front().c;
+		int cnt = q.front().cnt;
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int rr = r + dr[i];
+			int cc = c + dc[i];
+
+			if (rr < 0 || cc < 0 || rr >= n || cc >= m) continue;
+			if (visit[rr][cc]) continue;
+			if (arr[rr][cc] == 'W') continue;
+
+			dist = max(dist, cnt + 1);
+			visit[rr][cc] = true;
+			q.push({ rr, cc, cnt + 1 });
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++) cin >> arr[i][j];
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++) 
+			if (arr[i][j] == 'L') BFS(i, j);
+
+	cout << dist;
+}
