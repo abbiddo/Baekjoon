140502b37d599007f8a059386179ce8b0ad52ab1
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-07-02 23:13:04 +0900

    [Gold II] Title: 전기가 부족해, Time: 116 ms, Memory: 8740 KB -BaekjoonHub

--- 백준/Gold/10423. 전기가 부족해/전기가 부족해.cc
+++ 백준/Gold/10423. 전기가 부족해/전기가 부족해.cc
@@ -4,9 +4,7 @@ using namespace std;
 
 int n, m, k, res;
 bool visit[1001];
-bool kisit[1001];
 vector<pair<int, int>> road[100001];
-char uni[1001];
 
 priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
@@ -27,12 +25,10 @@ void prim() {
 }
 
 int main() {
-	priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> q;
 	cin >> n >> m >> k;
 	for (int i = 1; i <= k; i++) {
 		int a;	cin >> a;
 		visit[a] = true;
-		kisit[a] = true;
 	}
 
 	for (int i = 0; i < m; i++) {
@@ -40,16 +36,10 @@ int main() {
 		road[a].push_back({ c, b });
 		road[b].push_back({ c, a });
 
-		if (visit[a]) q.push({ c, {a, b} });
-		else if (visit[b]) q.push({ c, {b, a} });
+		if (visit[a]) pq.push({ c, b });
+		else if (visit[b]) pq.push({ c, a });
 	}
 
-	while (!q.empty() && k) {
-		if (kisit[q.top().second.first]) 
-			pq.push({ q.top().first, q.top().second.second });
-		q.pop();
-	}
-	
 	prim();
 
 	cout << res;
