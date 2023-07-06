f1c9c8d4e87e9c4ededd9092e919379f65d85a52
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-07-06 15:12:20 +0900

    [Bronze II] Title: OX퀴즈, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int n, c, sum;
+string s;   
+
+int main(){
+    cin>>n;
+    for (int i = 0; i < n; i++){
+        cin>>s;
+        c = 0; sum = 0;
+        for (int j = 0; j < s.size(); j++){
+            if (s[j] == 'O') { c++; sum += c; }
+            else c = 0;
+        }
+        cout<<sum<<"\n";
+    }
+}
