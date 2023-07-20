780bb4cfa4e3dbc329bdd06c20042a81a9afc5f2
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-07-20 11:45:31 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,30 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int n,m;
+int n, m;
 int arr[9];
-int vs[9];
+bool visit[9];
 
-void dfs(int num, int cnt){
-	if (cnt==m){
-		for (int i=0;i<m;i++) cout<<arr[i]<<" ";
-		cout<<"\n";
+void DFS(int num, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) cout << arr[i] << " ";
+		cout << "\n";
 		return;
 	}
-	for (int i=num;i<=n;i++){
-		if (!vs[i]){
-			vs[i]=1;
-			arr[cnt]=i;
-			dfs(i+1,cnt+1);
-			vs[i]=0;
+
+	for (int i = num; i <= n; i++){
+			arr[cnt] = i;
+			DFS(i + 1, cnt + 1);
 		}
-	}
 }
 
-int main(){
+int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	
-	cin>>n>>m;
-	dfs(1,0);
+
+	cin >> n >> m;
+	DFS(1, 0);
 }
