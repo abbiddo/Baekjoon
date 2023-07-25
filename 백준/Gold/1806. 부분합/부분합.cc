3482dafe370523ac3d665d9790b4ed6c4255cd84
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-07-25 12:38:58 +0900

    [Gold IV] Title: 부분합, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int n, s, res = 1000000;
+int arr[100000];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);	cout.tie(0);
+	
+	cin >> n >> s;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+
+	int left = 0, right = 0;
+	int sum = arr[0];
+
+	while (left < n) {
+		if (sum < s) {
+			if (right >= n) break;
+			right++;
+			sum += arr[right];
+		}
+		else {
+			if (left == right) {
+				cout << 1;
+				return 0;
+			}
+			res = min(res, (right - left + 1));
+			sum -= arr[left];
+			left++;
+		}
+	}
+	if (res == 1000000) cout << 0;
+	else cout << res;
+}
