acbf2ae83d731e87145921f369ba168cb935e563
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-07-25 18:29:46 +0900

    [Gold IV] Title: 스도쿠, Time: 88 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -1,59 +1,58 @@
 #include <iostream>
-#include <queue>
 using namespace std;
-
-bool complete;
-int zeroCnt;
-int board[9][9];
-pair<int, int> zero[81];
-
-bool check(int r, int c, int n) {
-	for (int i = 0; i < 9; i++)
-		if (board[r][i] == n || board[i][c] == n) return false;
-
-	for (int i = r / 3 * 3; i < r / 3 * 3 + 3; i++)
-		for (int j = c / 3 * 3; j < c / 3 * 3 + 3; j++)
-			if (board[i][j] == n) return false;
-
-	return true;
+int a[10][10];
+bool c[10][10];
+bool c2[10][10];
+bool c3[10][10];
+int n=9;
+int cnt=0;
+int square(int x, int y) {
+    return (x/3)*3+(y/3);
 }
-
-void sudoku(int cnt) {
-	if (cnt == zeroCnt) {
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++)
-				cout << board[i][j] << " ";
-			cout << "\n";
-		}
-		complete = true;
-		return;
-	}
-
-	int r = zero[cnt].first;
-	int c = zero[cnt].second;
-
-	for (int i = 1; i <= 9; i++) {
-		if (check(r, c, i)) {
-			board[r][c] = i;
-			sudoku(cnt + 1);
-		}
-		if (complete) return;
-	}
-
-	board[r][c] = 0;
+bool go(int z) {
+    cnt += 1;
+    if (cnt >= 10000000) {
+        return true;
+    }
+    if (z == 81) {
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                cout << a[i][j] << ' ';
+            }
+            cout << '\n';
+        }
+        return true;
+    }
+    int x = z/n;
+    int y = z%n;
+    if (a[x][y] != 0) {
+        return go(z+1);
+    } else {
+        for (int i=1; i<=9; i++) {
+            if (c[x][i] == 0 && c2[y][i] == 0 && c3[square(x,y)][i]==0) {
+                c[x][i] = c2[y][i] = c3[square(x,y)][i] = true;
+                a[x][y] = i;
+                if (go(z+1)) {
+                    return true;
+                }
+                a[x][y] = 0;
+                c[x][i] = c2[y][i] = c3[square(x,y)][i] = false;
+            }
+        }
+    }
+    return false;
 }
-
 int main() {
-	// 입출력 속도 향상을 위함
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
-			cin >> board[i][j];
-			if (board[i][j] == 0) zero[zeroCnt++] = { i, j };
-		}
-	}
-
-	sudoku(0);
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            cin >> a[i][j];
+            if (a[i][j] != 0) {
+                c[i][a[i][j]] = true;
+                c2[j][a[i][j]] = true;
+                c3[square(i,j)][a[i][j]] = true;
+            }
+        }
+    }
+    go(0);
+    return 0;
 }
