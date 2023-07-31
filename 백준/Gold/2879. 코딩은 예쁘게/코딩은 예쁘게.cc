691194c4984b32bf58ba09d287cfef0df52816d9
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-07-31 15:58:17 +0900

    [Gold III] Title: 코딩은 예쁘게, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int n, res;
+int arr[1000];
+int result[1000];
+
+int main() {
+	// 입출력 속도 향상을 위함
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	for (int i = 0; i < n; i++) cin >> result[i];
+
+	int gap = 0;
+	for (int i = 0; i < n; i++) {
+		int tmp = result[i] - arr[i];
+		if (tmp > 0) {
+			if (gap < 0) {
+				res += tmp;
+				gap = tmp;
+			}
+			else {
+				if (gap >= tmp) gap = tmp;
+				else {
+					res += (tmp - gap);
+					gap = tmp;
+				}
+			}
+		}
+		else {
+			if (gap > 0) {
+				res -= tmp;
+				gap = tmp;
+			}
+			else {
+				if (gap <= tmp) gap = tmp;
+				else {
+					res += (gap - tmp);
+					gap = tmp;
+				}
+			}
+		}
+	}
+	cout << res;
+}
