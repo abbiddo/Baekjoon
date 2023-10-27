097fad4e0de3745b930af9ac65facb67bf19877d
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-10-28 03:19:12 +0900

    [Gold V] Title: 다이어트, Time: 80 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Gold/1484. 다이어트/다이어트.java
+++ 백준/Gold/1484. 다이어트/다이어트.java
@@ -4,19 +4,18 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    static int n;
-    static int cnt;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n;
         n = Integer.parseInt(br.readLine());
 
-        long left = 1;
-        long right = 2;
+        int left = 1;
+        int right = 2;
+        int cnt = 0;
 
-        while (left <= n && right <= n && left <= right){
-            long sum = (right * right) - (left * left);
+        while (left <= (n+1)/2 && right <= (n+1)/2 && left <= right){
+            int sum = (right * right) - (left * left);
             if (sum == n){
                 System.out.println(right);
                 cnt++;
