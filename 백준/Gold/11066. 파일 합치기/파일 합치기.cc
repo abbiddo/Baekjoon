f7599f8eef4a8235f737a65538413cf2ba3925fb
abbiddo <79645200+abbiddo@users.noreply.github.com>
2023-12-30 22:47:20 +0900

    [Gold III] Title: 파일 합치기, Time: 132 ms, Memory: 3004 KB -BaekjoonHub

---
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int t, n;
+int arr[501];
+int dp[501][501];
+
+int main() {
+	cin >> t;
+	while (t--) {
+		cin >> n;
+
+		for (int i = 2; i <= n; i++)
+			for (int j = 1; j <= n; j++) dp[i][j] = INT_MAX;
+
+		for (int i = 1; i <= n; i++) {
+			cin >> dp[0][i];
+			arr[i] = arr[i - 1] + dp[0][i];
+		}
+		for (int i = 2; i <= n; i++) dp[2][i] = dp[0][i] + dp[0][i - 1];
+
+		for (int i = 3; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (j >= i) {
+					int tmp = arr[j] - arr[j - i];
+
+					for (int k = i - 1; k > 0; k--) {
+						dp[i][j] = min(dp[i][j], tmp + dp[k][j] + dp[i - k][j - k]);
+					}
+				}
+			}
+		}
+		cout << dp[n][n] << "\n";
+	}
+
+}
