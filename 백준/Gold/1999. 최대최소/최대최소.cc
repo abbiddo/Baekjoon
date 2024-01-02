a8b54898c819436da713bc2f13d74fead63bd145
abbiddo <79645200+abbiddo@users.noreply.github.com>
2024-01-02 23:36:38 +0900

    [Gold III] Title: 최대최소, Time: 100 ms, Memory: 2508 KB -BaekjoonHub

--- 백준/Gold/1999. 최대최소/최대최소.cc
+++ 백준/Gold/1999. 최대최소/최대최소.cc
@@ -6,29 +6,38 @@ int board[250][250];
 int arr[250][250];
 
 int main() {
-	ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
+
 	cin >> n >> b >> k;
 	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++) cin >> board[i][j];
-
-	for (int i = 0; i <= n - b; i++) {
-		for (int j = 0; j <= n - b; j++) {
-			int mi = 250;
-			int ma = 0;
-			for (int ii = i; ii < i + b; ii++) {
-				for (int jj = j; jj < j + b; jj++) {
-					mi = min(mi, board[ii][jj]);
-					ma = max(ma, board[ii][jj]);
-				}
-			}
-			arr[i][j] = ma - mi;
+		for (int j = 0; j < n; j++) {
+			cin >> board[i][j];
+			arr[i][j] = -1;
 		}
-	}
-
-
+    
 	while (k--) {
 		int r, c;	cin >> r >> c;
-		cout << arr[r - 1][c - 1] << "\n";
+		r--;
+		c--;
+
+		if (arr[r][c] >= 0) {
+			cout << arr[r][c] << "\n";
+			continue;
+		}
+
+		int mi = 250;
+		int ma = 0;
+
+		for (int ii = r; ii < r + b; ii++) {
+			for (int jj = c; jj < c + b; jj++) {
+				mi = min(mi, board[ii][jj]);
+				ma = max(ma, board[ii][jj]);
+			}
+		}
+
+		arr[r][c] = ma - mi;
+
+		cout << ma- mi << "\n";
 	}
 }
