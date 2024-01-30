e5a57e5708a84d78b693ee62481b74b22748ceae
abbiddo <79645200+abbiddo@users.noreply.github.com>
2024-01-30 20:10:32 +0900

    [Gold V] Title: 피자 굽기, Time: 584 ms, Memory: 89804 KB -BaekjoonHub

--- 백준/Gold/1756. 피자 굽기/피자 굽기.java
+++ 백준/Gold/1756. 피자 굽기/피자 굽기.java
@@ -1,21 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         Stack<Integer> s = new Stack<>();
 
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            int k = scanner.nextInt();
+            int k = Integer.parseInt(st.nextToken());
             if (!s.isEmpty() && s.peek() < k) k = s.peek();
             s.push(k);
         }
 
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            int a = scanner.nextInt();
+            int a = Integer.parseInt(st.nextToken());
             while (!s.isEmpty() && s.peek() < a) s.pop();
             if (!s.isEmpty()) s.pop();
             else {
